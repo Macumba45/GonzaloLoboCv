244a1bdcf7029bcc8d48e1d760691b9a32530bed
Macumba45 <gonzalolovo@gmail.com>
2023-05-29 12:02:56 +0200

    style: height fixed

--- src/components/BootCampProjects/styles.ts
+++ src/components/BootCampProjects/styles.ts
@@ -24,7 +24,7 @@ export const Container = styled.div`
     width: 100%;
     justify-content: center;
     align-items: center;
-    height: 800px;
+    /* height: 800px; */
     @media screen and (max-width: 600px) {
     }
 `
