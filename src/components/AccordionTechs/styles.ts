c00d2e31653d1881695c259de699416ab0b95868
Macumba45 <gonzalolovo@gmail.com>
2023-05-29 12:07:57 +0200

    style: height fixed

--- src/components/AccordionTechs/styles.ts
+++ src/components/AccordionTechs/styles.ts
@@ -1,8 +1,8 @@
 import styled from 'styled-components'
 
 export const ImgTech = styled.img`
-    width: 2.5rem;
-    margin: 1rem;
+    width: 2rem;
+    margin: 0.5rem;
     @media screen and (max-width: 600px) {
         width: 2.1rem;
     }
